2507fa39ce0783d1b3ee79f7abbe97235b93df7d
kartagis <tolga@ozses.net>
2014-06-06 23:37:59 +0300

    Fixes #2: Changed the way classes are discovered.

--- js/mysu.js
+++ js/mysu.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
-  var urls = document.getElementsByClassName('url');
+  var urls = document.querySelectorAll('a.url');
   for(i=0, len=urls.length; i<len; i++) {
     urls[i].addEventListener('click', function() {
       chrome.tabs.create({url: this.href});
