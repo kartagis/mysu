651b95d56ce53cf36ced04e4c169867aed8bdd0b
kartagis <tolga@ozses.net>
2014-06-21 16:34:05 +0300

    .

--- js/mysu.js
+++ js/mysu.js
@@ -1,8 +1,19 @@
 window.addEventListener('load', function() {
-  var urls = document.querySelectorAll('a.url');
-  for(i=0, len=urls.length; i<len; i++) {
+  var urls= document.querySelectorAll('a.url');
+  for(i=0,len=urls.length;i<len; i++) {
     urls[i].addEventListener('click', function() {
       chrome.tabs.create({url: this.href});
     }, false);
   }
 }, false);
+
+chrome.contextMenus.create({'title': 'Add to mySU bookmarks', 'contexts': ['link', 'page'], 'onclick': mySUBookmark});
+
+function mySUBookmark(info, tab) {
+  var a = document.getElementsByTagName('');
+  for (i=0,len=a.length;i<len;i++) {
+    a[i].addEventListener('contextmenu', function() {
+      alert(this.textContent+' clicked!');
+    });
+  };
+};
